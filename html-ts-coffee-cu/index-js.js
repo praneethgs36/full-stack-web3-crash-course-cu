8662efb8cd1c0781f948e734d8258cddbdeb2308
praneethgs33 <your-email@example.com>
2025-05-25 11:29:17 +0530

    changes

---
+++ html-ts-coffee-cu/index-js.js
@@ -0,0 +1,11 @@
+const connectButton = document.getElementById("connectButton");
+
+function connect() {
+    if (typeof window.ethereum) != "undefined") {
+        console.log("hi");
+    } else {
+        connectButton.innerHTML = "Please install Metamask";
+    }
+}
+
+connectButton.onclick = connect();
