df11b50d496a8b1cb3b1a0d612f60c78f84b9970
Praneeth <praneethgs36@gmail.com>
2025-06-09 13:57:10 +0530

    new files

--- html-ts-coffee-cu/README.md
+++ html-ts-coffee-cu/README.md
@@ -4,5 +4,8 @@
 - The following buttons that will map to a solidity smart contract.
     - Connect
     - Buy Coffee
+        - Button to buy coffee
+        - Call a function on a smart contract
+        - Have a test blockchain we can call
     - Get Balance
     - Withdraw
--- html-ts-coffee-cu/index-js.js
+++ html-ts-coffee-cu/index-js.js
@@ -1,12 +1,47 @@
-const connectButton = document.getElementById("connectButton");
+import {createWalletClient, custom, createPublicClient} from "https://esm.sh/viem"
 
-function connect() {
-    if (typeof window.ethereum) != "undefined") {
-        console.log("hi");
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const ethAmountInput = document.getElementById("ethAmount");
+
+
+let walletClient;
+let publicClient;
+
+async function connect() {
+    if (typeof window.ethereum != "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+        await walletClient.requestAddresses();
+        connectButton.innerHTML = "Connected";
+
     } else {
         connectButton.innerHTML = "Please install Metamask";
     }
 }
 
+async function fund() {
+    const ethAmount = ethAmountInput.value;
+    console.log(`Funding with ${ethAmount}...`);
 
-connectButton.onclick = connect();
+    if (typeof window.ethereum != "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+        await walletClient.requestAddresses();
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        });
+
+        await publicClient.similateContract();
+
+    } else {
+        connectButton.innerHTML = "Please install Metamask";
+    }
+
+}
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
